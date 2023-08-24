921268cb62f2e972bfe98125437d299faa881721
kumail imran <kumail2306@gmail.com>
2023-08-24 17:20:52 +0100

    bug fix 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,19 +17,21 @@ export default async function Home() {
       <div className="text-center">
         <h2 className="hometitle">WELCOME TO THE GLASS RAINFOREST</h2>
         <p className="intro">
-          Embark on an expedition of discovery, where each post opens a door to
+          {`Embark on an expedition of discovery, where each post opens a door to
           the wonders that thrive within the depths of this rainforest. From
           rare and hidden animals that roam its dense undergrowth to ancient
           myths whispered by indigenous communities, we delve into the
           unexplored corners and untold narratives that make the Amazon
-          Rainforest a true enigma of the natural world. Whether you're a
-          curious explorer, a seeker of truths, or simply captivated by the
-          unknown, our blog is your portal to a tapestry of narratives that
-          illuminate the intricacies of our world. Embark with us on a voyage of
-          discovery, where each page turns over a new leaf of fascination and
-          every click uncovers the secrets that lie beneath the surface. Welcome
-          to a place where curiosity finds its home, and the extraordinary
-          unfolds in every word and image.
+          Rainforest a true enigma of the natural world.
+          <br />
+          <br />
+          Whether you're a curious explorer, a seeker of truths, or simply
+          captivated by the unknown, our blog is your portal to a tapestry of
+          narratives that illuminate the intricacies of our world. Embark with
+          us on a voyage of discovery, where each page turns over a new leaf of
+          fascination and every click uncovers the secrets that lie beneath the
+          surface. Welcome to a place where curiosity finds its home, and the
+          extraordinary unfolds in every word and image.`}
         </p>
         <div className="homepage">
           <div className="overlay"></div>
